c65991c602e72c1448f89e23b0e687b25262e13b
Marc B.D. Greenberg <junkformarc@gmail.com>
2015-03-12 12:10:17 -0700

    Initial setup
    basic files starting out with

---
+++ js/debate.js
@@ -0,0 +1,60 @@
+$( document ).ready(function() {
+
+   // Set up and draw the 1st level
+   setupGame(16);
+
+   // Attatch event handlers
+
+   // testcode
+
+});
+
+
+function setupGame(gameSize) {
+
+   // First do some cleanup
+   $(".matches").empty();
+   $("#brackets").css({"-webkit-transform": "scale(1)"});
+   $("#numTopics").html(gameSize);
+   
+   // Assign a random number to each set in the topics list
+   for (var i=0; i<Topics.length; i++) {
+      var num = Topics[i][0];  // taking into account the weight of each set
+      if (num == 0) {
+         Topics[i][0] = Math.random();
+      } else {
+         Topics[i][0] = num * Math.random();
+      }
+   }
+   
+   // Sort the topics list by the randomly generated values
+   Topics.sort( function(a,b) { return (a[0] < b[0]) ? 1 : ((b[0] < a[0]) ? -1 : 0); } );
+
+   // Set up each of the 4 brackets
+   var bracketSize = gameSize / 4;
+   for (var i=0; i<4; i++) {
+      var $bracket = $("#bracket" + (i+1)),
+            $matches = $bracket.find(".matches");
+      
+      for (var j=0; j<bracketSize/2; j++) {
+         var $match = $("<div class='match'></div>");
+         $match.append("<div class='topic'>" + Topics[j+(i*bracketSize/2)][1][0] + "</div>");
+         $match.append("<div class='versus'>vs.</div>");
+         $match.append("<div class='topic'>" + Topics[j+(i*bracketSize/2)][1][1] + "</div>");
+         $matches.append($match);
+      }
+   }
+   
+   // Zoom in on the top of the 1st bracket
+   // setTimeout(function(){
+   //    $("#brackets").css({"-webkit-transform": "scale(2)"});
+   // }, 2000);
+
+}
+
+/* -------------------- */
+/* Current state */
+var Current = {
+   //topicMap: {}
+}
+
